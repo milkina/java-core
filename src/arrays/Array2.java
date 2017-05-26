7906cd3bddf339d7fabd4876b96226e80d187e58
creativexi <av133x@gmail.com>
2017-05-26 15:42:17 +0300

    добавил распечатку для февраля

--- src/arrays/Array2.java
+++ src/arrays/Array2.java
@@ -4,5 +4,6 @@ public class Array2 {
     public static void main(String[] args) {
         int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         System.out.println("B апреле " + monthDays[3] + " дней.");
+        System.out.println("B феврале " + monthDays[1] + " дней.");
     }
 }
