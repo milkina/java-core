97c18eb873a077fed2594a6f06f81ca3ea27fa0c
milkina <tatyana.milkina@gmail.com>
2017-07-23 22:52:23 +0300

    1

---
+++ src/enumerations/CoffeeSize5.java
@@ -0,0 +1,7 @@
+package enumerations;
+
+public class CoffeeSize5 {
+    public static final String BIG = "BIG";
+    public static final String HUGE = "HUGE";
+    public static final String OVERWHELMING = "OVERWHELMING";
+}
