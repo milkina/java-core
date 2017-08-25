9fb9222198bac1fbea20acdfc8b38e6f7d055b3e
milkina <tatyana.milkina@gmail.com>
2017-08-25 14:22:02 +0300

    1

--- src/io/BufferedWriterDemo.java
+++ src/io/BufferedWriterDemo.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 
 public class BufferedWriterDemo {
     public static void main(String[] args) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("src\\io\\buffWriter.txt"))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src\\io\\buffWriter.txt"))) {
             String text = "Привет мир!";
-            bw.write(text);
-            bw.newLine();
-            bw.write(text);
+            bufferedWriter.write(text);
+            bufferedWriter.newLine();
+            bufferedWriter.write(text);
+            bufferedWriter.flush();
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
--- src/io/FileInputOutputStreamDemo.java
+++ src/io/FileInputOutputStreamDemo.java
@@ -6,13 +6,13 @@ public class FileInputOutputStreamDemo {
     public static void main(String args[]) {
         try {
             char c[] = {'a', 'b', 'c'};
-            OutputStream output = new FileOutputStream("file.txt"); // Создание текстового файла
+            OutputStream output = new FileOutputStream("a.txt"); // Создание текстового файла
             for (int i = 0; i < c.length; i++) {
                 output.write(c[i]); // Запись каждого символа в текстовый файл
             }
             output.close();
 
-            InputStream input = new FileInputStream("file.txt");
+            InputStream input = new FileInputStream("a.txt");
             int size = input.available();
 
             for (int j = 0; j < size; j++) {
