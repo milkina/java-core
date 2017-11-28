1b8d5040506526679c3c7690f77bd37faf671f4b
milkina <tatyana.milkina@gmail.com>
2017-11-28 20:10:53 +0200

    Fix issues

--- src/jdbc/InsertData.java
+++ src/jdbc/InsertData.java
@@ -1,19 +1,25 @@
 package jdbc;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-import static jdbc.ConnectionData.*;
+import static jdbc.ConnectionData.DRIVER;
+import static jdbc.ConnectionData.URL;
+import static jdbc.ConnectionData.USER;
+import static jdbc.ConnectionData.PASSWORD;
 
 public class InsertData {
-    private final static String insertQuery = "INSERT INTO users (username) VALUES ('sidorov');";
+    private static final String INSERT_QUERY =
+            "INSERT INTO users (username) VALUES ('sidorov');";
 
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName(DRIVER);
-        try (java.sql.Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        try (Connection connection =
+                     DriverManager.getConnection(URL, USER, PASSWORD);
              Statement statement = connection.createStatement()) {
 
-            statement.execute(insertQuery);
+            statement.execute(INSERT_QUERY);
         } catch (Exception e) {
             e.printStackTrace();
         }
