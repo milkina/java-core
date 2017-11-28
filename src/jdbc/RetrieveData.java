4880ba53ea07fc71ebd2804e17def9043f3ff069
milkina <tatyana.milkina@gmail.com>
2017-11-28 19:53:32 +0200

    Fix issues

--- src/jdbc/RetrieveData.java
+++ src/jdbc/RetrieveData.java
@@ -4,23 +4,27 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-import static jdbc.ConnectionData.*;
+import static jdbc.ConnectionData.DRIVER;
+import static jdbc.ConnectionData.URL;
+import static jdbc.ConnectionData.USER;
+import static jdbc.ConnectionData.PASSWORD;
 
 public class RetrieveData {
-    private final static String selectQuery = "SELECT * FROM users;";
+    private static final String SELECT_QUERY = "SELECT * FROM users;";
 
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName(DRIVER);
-        try (java.sql.Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        try (java.sql.Connection connection =
+                     DriverManager.getConnection(URL, USER, PASSWORD);
              Statement statement = connection.createStatement()) {
 
-            ResultSet resultSet = statement.executeQuery(selectQuery);
-            System.out.printf("%-20s%s\n", "id", "username");
+            ResultSet resultSet = statement.executeQuery(SELECT_QUERY);
+            System.out.printf("%-20s%s%n", "id", "username");
             System.out.println("-------------------------------");
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("username");
-                System.out.printf("%-20d%s\n", id, name);
+                System.out.printf("%-20d%s%n", id, name);
             }
         } catch (Exception e) {
             e.printStackTrace();
