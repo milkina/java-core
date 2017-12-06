60a5dcabc692eb7529d572f8e3fe673b48fb12c0
milkina <tatyana.milkina@gmail.com>
2017-12-06 11:54:13 +0200

    Added h2 support.

--- src/jdbc/ConnectionData.java
+++ src/jdbc/ConnectionData.java
@@ -1,9 +1,13 @@
 package jdbc;
 
 public class ConnectionData {
-    public static final String DRIVER = "com.mysql.jdbc.Driver";
-    public static final String DB = "catalog";
-    public static final String URL = "jdbc:mysql://localhost:3306/" + DB;
-    public static final String USER = "root";
-    public static final String PASSWORD = "admin";
+    //  public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DRIVER = "org.h2.Driver";
+    public static final String DB = "test";
+    //    public static final String URL = "jdbc:mysql://localhost:3306/" + DB;
+    public static final String URL = "jdbc:h2:tcp://localhost/~/" + DB;
+    //public static final String USER = "root";
+    public static final String USER = "sa";
+    // public static final String PASSWORD = "admin";
+    public static final String PASSWORD = "";
 }
--- src/jdbc/CreatingTable.java
+++ src/jdbc/CreatingTable.java
@@ -11,10 +11,10 @@ import static jdbc.ConnectionData.PASSWORD;
 
 public class CreatingTable {
     private static final String CREATE_TABLE_QUERY =
-            "CREATE TABLE `users` "
-                    + "(`id` INT(5) NOT NULL AUTO_INCREMENT,"
-                    + " `username` VARCHAR(50), "
-                    + "PRIMARY KEY(`id`));";
+            "CREATE TABLE users "
+                    + "(id INT(5) NOT NULL AUTO_INCREMENT,"
+                    + " username VARCHAR(50), "
+                    + "PRIMARY KEY(id));";
 
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName(DRIVER);
