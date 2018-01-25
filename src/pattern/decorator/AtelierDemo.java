175e148cf1d91da2a6c473fe1f3716ca86ad9b12
milkina <tatyana.milkina@gmail.com>
2018-01-25 14:35:46 +0200

    Decorator for clothes

--- src/pattern/decorator/AtelierDemo.java
+++ src/pattern/decorator/AtelierDemo.java
@@ -5,7 +5,7 @@ public class AtelierDemo {
         ClothI tie = new Tie(Size.S, 56, "Black");
         ClothI wrappedTie = new PostDelivery(new SimpleWrapperDecorator(tie));
         wrappedTie.printDetailedPrice();
-        System.out.println("Итого: " + wrappedTie.calculatePrice()+ " грн");
+        System.out.println("Итого: " + wrappedTie.calculatePrice() + " грн");
         wrappedTie.printDetailedTime();
         System.out.println("Итого: " + wrappedTie.calculateTime() + " дня");
 
@@ -14,7 +14,7 @@ public class AtelierDemo {
         ClothI tShirt = new TShirt(Size.XS, 78, "White");
         ClothI wrappedTShirt = new SimpleWrapperDecorator(tShirt);
         wrappedTShirt.printDetailedPrice();
-        System.out.println("Итого: " + wrappedTShirt.calculatePrice()+" грн.");
+        System.out.println("Итого: " + wrappedTShirt.calculatePrice() + " грн.");
         wrappedTShirt.printDetailedTime();
         System.out.println("Итого: " + wrappedTShirt.calculateTime() + " дня");
     }
