f36c3b63fd8a2dddb793bb9911e549dd16e53ed9
milkina <tatyana.milkina@gmail.com>
2019-01-16 12:13:56 +0200

    Some changes

--- src/strings/StringDemo5.java
+++ src/strings/StringDemo5.java
@@ -4,6 +4,7 @@ public class StringDemo5 {
     public static void main(String[] args) {
         System.out.println("Foobar".endsWith("bar"));
         System.out.println("Foobar".startsWith("Foo"));
+        System.out.println("Foobar".contains("oo"));
         System.out.println("Hello world!!!".regionMatches(6, "world", 0, 3));
     }
 }
