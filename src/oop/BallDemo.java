5ca6ba39ee15e1457f6a1f1685e29ca04e1eb102
milkina <tatyana.milkina@gmail.com>
2019-03-13 16:21:16 +0200

    Some changes

--- src/oop/BallDemo.java
+++ src/oop/BallDemo.java
@@ -4,8 +4,7 @@ public class BallDemo {
     public static void main(String[] args) {
         Ball ball1 = new Ball("red");
         Ball ball2 = new Ball("blue");
-        System.out.println(Ball.getCountInstance());
-        System.out.println(ball1);
-        System.out.println(ball2);
+        System.out.println("Количество созданных объектов: "
+                + Ball.getCountInstance());
     }
 }
