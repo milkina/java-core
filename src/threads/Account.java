a6c178ea6db5b200ca608bea7cb0519f23a55065
milkina <tatyana.milkina@gmail.com>
2019-05-14 16:59:22 +0300

    Some changes

--- src/threads/Account.java
+++ src/threads/Account.java
@@ -1,6 +1,6 @@
 package threads;
 
-public class Account {
+public class Account implements Runnable {
     private int balance = 50;
 
     public int getBalance() {
@@ -8,6 +8,35 @@ public class Account {
     }
 
     public void withdraw(int amount) {
-        balance = balance - amount;
+        balance -= amount;
+    }
+
+    public void run() {
+        for (int x = 0; x < 5; x++) {
+            makeWithdrawal(10);
+            if (getBalance() < 0) {
+                System.out.println("account is overdrawn!");
+            }
+        }
+    }
+
+    private void makeWithdrawal(int amount) {
+        if (getBalance() >= amount) {
+            System.out.println(Thread.currentThread().getName()
+                    + " is going to withdraw");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            withdraw(amount);
+            System.out.println(Thread.currentThread().getName()
+                    + " completes the withdrawal. The balance is "
+                    + getBalance());
+        } else {
+            System.out.println("Not enough in account for "
+                    + Thread.currentThread().getName()
+                    + " to withdraw " + getBalance());
+        }
     }
 }
--- src/threads/AccountDanger.java
+++ src/threads/AccountDanger.java
@@ -1,44 +1,13 @@
 package threads;
 
-public class AccountDanger implements Runnable {
-    private Account account = new Account();
-
+public class AccountDanger{
     public static void main(String[] args) {
-        AccountDanger accountDanger = new AccountDanger();
-        Thread one = new Thread(accountDanger);
-        Thread two = new Thread(accountDanger);
+        Account account = new Account();
+        Thread one = new Thread(account);
+        Thread two = new Thread(account);
         one.setName("Fred");
         two.setName("Lucy");
         one.start();
         two.start();
-    }
-
-    public void run() {
-        for (int x = 0; x < 5; x++) {
-            makeWithdrawal(10);
-            if (account.getBalance() < 0) {
-                System.out.println("account is overdrawn!");
-            }
-        }
-    }
-
-    private void makeWithdrawal(int amt) {
-        if (account.getBalance() >= amt) {
-            System.out.println(Thread.currentThread().getName()
-                    + " is going to withdraw");
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-            account.withdraw(amt);
-            System.out.println(Thread.currentThread().getName()
-                    + " completes the withdrawal. The balance is "
-                    + account.getBalance());
-        } else {
-            System.out.println("Not enough in account for "
-                    + Thread.currentThread().getName()
-                    + " to withdraw " + account.getBalance());
-        }
     }
 }
