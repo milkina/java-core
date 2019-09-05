2bfbfebc45e781dfb1f79421b864f0a03374a801
milkina <tatyana.milkina@gmail.com>
2019-09-05 20:14:45 +0300

    Added fibonacci.

---
+++ src/arrays/Fibonacci.java
@@ -0,0 +1,42 @@
+package arrays;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.Period;
+import java.util.stream.LongStream;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 25;
+        LocalTime localTime1 = LocalTime.now();
+        System.out.print("Рекурсия " + recursive(n));
+        LocalTime localTime2 = LocalTime.now();
+        Duration duration1 = Duration.between(localTime1, localTime2);
+        System.out.println(", время выполнения - " + duration1);
+
+        LocalTime localTime3 = LocalTime.now();
+        System.out.print("Цикл for " + calculateWithFor(n));
+        LocalTime localTime4 = LocalTime.now();
+        Duration duration2 = Duration.between(localTime3, localTime4);
+        System.out.println(", время выполнения - " + duration2);
+    }
+
+    private static long calculateWithFor(int n) {
+        long first = 0;
+        long second = 1;
+        long result = n;
+        for (int i = 1; i < n; i++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return result;
+    }
+
+    private static long recursive(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return recursive(n - 2) + recursive(n - 1);
+    }
+}
